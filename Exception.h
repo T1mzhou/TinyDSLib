e630c5ea95c37eb30322d616a7d1e3c26716f784
T1mzhou <zhoutianwenzzza@outlook.com>
2021-11-22 23:41:17 +0800

    update Exception

--- Exception.h
+++ Exception.h
@@ -24,5 +24,72 @@ protected:
     char* m_message;
     char* m_location;
 };
+
+class ArithmeticException : public Exception
+{
+    ArithmeticException(const char* message) : Exception(message)  { }
+    ArithmeticException(char* file, int line) : Exception(file, line) { }
+    ArithmeticException(const char* message, const char* file, int line) : Exception(message, file, line) { }
+    ArithmeticException(const ArithmeticException& e) :  Exception(e) { }
+    ArithmeticException& operator=(const ArithmeticException& e)
+    {
+        Exception::operator=(e);
+        return *this;
+    }
+};
+
+class NullPointerException : public Exception
+{
+    NullPointerException(const char* message) : Exception(message)  { }
+    NullPointerException(char* file, int line) : Exception(file, line) { }
+    NullPointerException(const char* message, const char* file, int line) : Exception(message, file, line) { }
+    NullPointerException(const NullPointerException& e) :  Exception(e) { }
+    NullPointerException& operator=(const NullPointerException& e)
+    {
+        Exception::operator=(e);
+        return *this;
+    }
+};
+
+
+class IndexOutOfBoundsException : public Exception
+{
+    IndexOutOfBoundsException(const char* message) : Exception(message)  { }
+    IndexOutOfBoundsException(char* file, int line) : Exception(file, line) { }
+    IndexOutOfBoundsException(const char* message, const char* file, int line) : Exception(message, file, line) { }
+    IndexOutOfBoundsException(const IndexOutOfBoundsException& e) :  Exception(e) { }
+    IndexOutOfBoundsException& operator=(const IndexOutOfBoundsException& e)
+    {
+        Exception::operator=(e);
+        return *this;
+    }
+};
+
+class NoEnoughMemoryException : public Exception
+{
+    NoEnoughMemoryException(const char* message) : Exception(message)  { }
+    NoEnoughMemoryException(char* file, int line) : Exception(file, line) { }
+    NoEnoughMemoryException(const char* message, const char* file, int line) : Exception(message, file, line) { }
+    NoEnoughMemoryException(const NoEnoughMemoryException& e) :  Exception(e) { }
+    NoEnoughMemoryException& operator=(const NoEnoughMemoryException& e)
+    {
+        Exception::operator=(e);
+        return *this;
+    }
+};
+
+class InvalidParameterException : public Exception
+{
+    InvalidParameterException(const char* message) : Exception(message)  { }
+    InvalidParameterException(char* file, int line) : Exception(file, line) { }
+    InvalidParameterException(const char* message, const char* file, int line) : Exception(message, file, line) { }
+    InvalidParameterException(const InvalidParameterException& e) :  Exception(e) { }
+    InvalidParameterException& operator=(const InvalidParameterException& e)
+    {
+        Exception::operator=(e);
+        return *this;
+    }
+};
+
 }
 #endif // !__EXCEPTION_H__
