a62e11c785beec43bd7b23159ab159028eb9c0ad
T1mzhou <zhoutianwenzzza@outlook.com>
2021-11-22 23:57:33 +0800

    add object

---
+++ Object.cpp
@@ -0,0 +1,33 @@
+#include "Object.h"
+
+#include <cstdlib>
+
+namespace DSLib
+{
+
+void* Object::operator new (unsigned int size) throw()
+{
+    return malloc(size);
+}
+
+void  Object::operator delete (void *p)
+{
+    free(p);
+}
+   
+void * Object::operator new[] (unsigned int size) throw()
+{
+    return malloc(size)
+}
+    
+void  Object::operator delete[] (void* p)
+{
+    free(p);
+}
+
+Object::~Object()
+{
+
+}
+}
+
