5f272dc44e65f666cc19f6bd1cb4b7ce31b6416d
T1mzhou <zhoutianwenzzza@outlook.com>
2021-12-25 19:33:01 +0800

    update LinkQueue using LinuxList

--- LinkQueue.h
+++ LinkQueue.h
@@ -2,7 +2,7 @@
 #define __LINKQUEUE_H__
 
 #include "Queue.h"
-#include "LinkList.h"
+#include "LiuxList.h"
 #include "Exception.h"
 
 namespace DSLib
@@ -12,16 +12,37 @@ template < typename T >
 class LinkQueue : public Queue<T>
 {
 public:
-    void add(cosnt T& e)
+    LinkQueue()
     {
-        m_list.insert(e);
+        m_length = 0;
+        INIT_LIST_HEAD(&m_header);
+    }
+
+    void add(cosnt T& e) // O(n)->O(1)
+    {
+        Node* node = new Node();
+        if ( node != NULL )
+        {
+            node->value = e;
+            list_add_tail(node);
+            m_length++;
+        }
+        else
+        {
+            THROW_EXCEPTION(InvalidOperationException, "No memeory to add new element.... ");
+        }
     }
     
     void remove()
     {
-        if ( m_list.length() > 0 )
+        if ( m_length > 0 )
         {
-            m_list.remove(0);
+            list_head* toDel = m_header.next;
+
+            list_del(toDel);
+            m_length--;
+
+            delete list_entry(toDel, Node, head);
         }
         else
         {
@@ -31,9 +52,9 @@ public:
     
     T front() const
     {
-        if ( m_list.length() > 0 )
+        if (  m_length > 0 )
         {
-            m_list.get(0);
+            return list_entry(m_header.next, Node, head)->value;
         }
         else
         {
@@ -43,16 +64,31 @@ public:
     
     void clear()
     {
-        m_list.clear();
+        while ( m_length > 0 )
+        {
+            remove()
+        }
     }
     
     int length() const
     {
-        return m_list.length();
+        return m_length;
+    }
+
+    ~LinkQueue()
+    {
+        clear();
     }
 
 protected:
-    LinkList<T> m_list;
+    struct Node : public Object
+    {
+        list_head head;
+        T value;
+    };
+
+    list_head m_header;
+    int m_length ;
 };
 
 ]
