b6db33ffdb9e7be9b9fa911230dee43371c29a5d
T1mzhou <zhoutianwenzzza@outlook.com>
2022-01-04 22:52:46 +0800

    add merge sort and quick sort

--- Sort.h
+++ Sort.h
@@ -102,6 +102,25 @@ public:
         } while (d > 1);
         
     }
+
+    template < typename T >
+    static void Merge(T array[], int len, bool min2max = true)
+    {
+        T* helper = new T[len];
+        
+        if ( helper != NULL )
+        {
+            Merge(array, helper, 0, len - 1, true);
+        }
+
+        delete helper;
+    }
+
+    template < typename T >
+    static void Quick(T array[], int len, bool min2max = true)
+    {
+       Quick(array, 0, len, true);
+    }
 private:
     Sort();
     Sort(const Sort&);
@@ -113,6 +132,94 @@ private:
         T c(a);
         a = b;
         b = c;
+    }
+
+    template < typename T >
+    static void Merge(T src[], T helper[], int begin, int end, bool min2max = true)
+    {
+        if ( begin == end )
+        {
+            return ;
+        }
+
+        int mid = (begin + end) / 2;
+        Merge(src, helper, begin, mid, min2max);
+        Merge(src, helper, mid + 1, end, min2max);
+        Merge(src, helper, begin, mid ,end, min2max);
+    }
+
+    template < typename T >
+    static void Merge(T src[], T helpher, int begin, int mid, int end, bool min2max = true)
+    {
+        int i = begin;
+        int j = mid + 1;
+        int k = begin;
+
+        while ( (i <= mid) && (j <= end) )
+        {
+            if ( min2max ? (src[i] < src[j]) : (src[i] > src[j]) )
+            {
+                helper[k++] = src[i++];
+            }
+            else
+            {
+                helper[k++] = src[i++];
+            }
+        }
+
+        while ( i <= mid)
+        {
+            helper[k++] = src[i++];
+        }
+
+        while ( j <= end )
+        {
+            helper[k++] = src[j++];
+        }
+
+        for (int i = begin; i <= end; i++)
+        {
+            src[i] = helper[i];
+        }
+    }
+
+    template < typename T >
+    static int Partition(T array[], int begin, int end, bool min2max)
+    {
+        T pv = array[begin];
+
+        while ( begin < end )
+        {
+            while ( (begin < end) && (min2max ? (array[end] > pv) : (array[end] < pv) )
+            {
+                end--;
+            }
+
+            swap( array[begin], array[end]);
+
+            while ( (begin < end) && (min2max ? (array[begin] <= pv) : (array[begin] >= pv)) )
+            {
+                begin++;
+            }
+
+            swap( array[begin], array[end]);
+
+        }
+
+        array[begin] = pv;
+
+        return begin;
+    }
+
+    template < typename T >
+    static void Quick(T array[], int begin, int end, bool min2max)
+    {
+        if ( begin < end )
+        {
+            int pivot = Partition(array, begin, end, min2max);
+            Quick(array, begin, pivot - 1, min2max);
+            Quick(array, pivot, end, min2max);
+        }
     }
 };
 
